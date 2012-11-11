239008fbed74132a441032fd17b1f37ffb58e3f3
Shruti Kulkarni <sgk2118@columbia.edu>
2012-11-11 11:17:32 -0500

    geolocation works!!!!!

--- js/findPlaces.js
+++ js/findPlaces.js
@@ -4,7 +4,6 @@ function printLocs(lati, longi, query){
 	if (longi.toString() == 'NaN' || longi.toString() == 'null' || longi === 0)
 		longi = -74.3164;
 	var url = 'https://api.foursquare.com/v2/venues/search?ll='+lati+','+longi+'&query='+query+'&client_id=2POUFAUU4ZBJ2MTDOY3S2YHR2NIT52FYW0LUTPHBMNTJFJNQ&client_secret=YFDZI1YWV3ZI5S5SPM2DZJEQIEBPIDJ5XFZBWTIKIQZVQNYM&v=20120101';
-	console.log(url);
 
 		$.getJSON(url,
 		    function(data) {

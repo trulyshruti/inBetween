ffcfc1a1404da0b87927c92b6f60e3bbc3bb13d0
Shruti Kulkarni <sgk2118@columbia.edu>
2012-11-11 10:14:23 -0500

    new changes

--- js/findPlaces.js
+++ js/findPlaces.js
@@ -10,23 +10,29 @@ function printLocs(lati, longi, query){
 		        $.each(data.response.venues, function(i,venues){
 		            content = '<center>';
 		            content += '<p> <br />';
+		            addy = '';
 					if(venues.name != null) {
 						content += '&nbsp;' + venues.name + '<br />';
 					}
 					if(venues.location.address != null) {
 						content += '&nbsp;' + venues.location.address + '<br />';
+						addy += venues.location.address + ' ';
 					}
 					if(venues.location.crossStreet != null) {
 						content += ' &nbsp;' + venues.location.crossStreet + '<br />';
+						addy += venues.location.crossStreet + ' ';
 					}
 					if(venues.location.city != null) {
 						content += ' &nbsp;' + venues.location.city + ', ';
+						addy += ', ' + venues.location.city + '';
 					}
 					if(venues.location.state != null) {
 						content += ' &nbsp;' + venues.location.state + '';
+						addy += ', ' + venues.location.address state+ ' ';
 					}
 					if(venues.location.postalCode != null) {
 						content += ' &nbsp;' + venues.location.postalCode + '';
+						addy += venues.location.postalCode + ' ';
 					}
 					if(venues.location.distance != null) {
 						content += '<br /> &nbsp; Distance: ' + venues.location.distance + '<br />';
@@ -34,6 +40,8 @@ function printLocs(lati, longi, query){
 					if(venues.contact.phone != null) {
 						content += '&nbsp;' + venues.location.phone + '<br />';
 					}
+					content += '<a href=';
+					content += 'Find on Google Maps!></a>';
 					content += '</p> </center>';
 					$(content).appendTo("#names");
 		       });
